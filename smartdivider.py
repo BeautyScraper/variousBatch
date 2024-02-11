c59784dfa5af115e839861db4e4813ccec137ad2
HP <sstico0@gmail.com>
2024-02-11 12:47:29 +0530

    taking input from csv

--- smartdivider.py
+++ smartdivider.py
@@ -39,12 +39,22 @@ def dividesmartly(targetPath,filemovingdict):
     for key,tp in filemovingdict.items():
         dividesmartly_helper(key,tp,targetPath)
 
-def main():
+def main2():
     targetPath = r'D:\paradise\stuff\new\FS'
     filemovingdict = {'1.txt':r'D:\paradise\stuff\Essence\FS\yummyClips\deletable',
     '2.txt':r'D:\paradise\stuff\Essence\FS\yummyClips\TheekThak',
     '3.txt':r'D:\paradise\stuff\Essence\FS\yummyClips\SachMe',
     '6.txt':r'C:\dumpinggrounds\known_loved_video.txt'}
     dividesmartly(targetPath,filemovingdict)
-    
-main()
+
+def main():
+    # Read the CSV file into a pandas DataFrame
+    df = pd.read_csv('distributer.csv')
+    # breakpoint()
+    # Iterate over each row in the DataFrame and apply dividesmartly function
+    for index, row in df.iterrows():
+        dividesmartly_helper(row['filename'], row['tp'], row['targetPath'])
+
+if __name__ == "__main__":
+    main()
+    main2()
